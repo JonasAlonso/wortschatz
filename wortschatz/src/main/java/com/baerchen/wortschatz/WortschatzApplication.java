63103c93a03092dedb8ae6b19d04036ef396faae
JonasAlonso <jonasalonsolanza@gmail.com>
2025-06-26 17:54:41 +0200

    WS-101 first commit

---
+++ wortschatz/src/main/java/com/baerchen/wortschatz/WortschatzApplication.java
@@ -0,0 +1,13 @@
+package com.baerchen.wortschatz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WortschatzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WortschatzApplication.class, args);
+	}
+
+}
---
+++ wortschatz/src/test/java/com/baerchen/wortschatz/WortschatzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baerchen.wortschatz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WortschatzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
